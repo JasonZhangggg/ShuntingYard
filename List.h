0bca9893989f62c22814af25fb7c75b73759c36a
U-BSD\383349 <383349@SHS-10DEC4E04.beaverton.k12.or.us>
2020-02-10 10:46:16 -0800

    third commit

---
+++ List.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#include "Node.h"
+#ifndef LIST_H
+#define LIST_H
+
+using namespace std;
+
+class List{
+ public:
+  List(char*);
+  void push(char*);
+  char* s_pop();
+  char* q_pop();
+  char* s_peek();
+  char* q_peek();
+  char* get_end(Node*&, bool);
+  char* copy(char*);
+  ~List();
+ private:
+  Node* head;
+};
+#endif
---
+++ Node.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+#include "Node.h"
+
+using namespace std;
+
+//Constructor
+Node::Node(char* newVal) {
+  val = new char[strlen(newVal)+1];
+  strcpy(val, newVal);
+  next = NULL;
+  right = NULL;
+  left = NULL;
+}
+
+char* Node:: getVal(){
+  return val;
+}
+void Node::setNext(Node* newNext){
+  next = newNext;
+}
+
+Node* Node::getNext(){
+  return next;
+}
+void Node::setRight(Node* newRight){
+  right = newRight;
+}
+Node* Node::getRight(){
+  return right;
+}
+void Node::setLeft(Node* newLeft){
+  left = newLeft;
+}
+Node* Node::getLeft(){
+  return left;
+}
+
+Node::~Node(){
+  delete[] val;
+  next = NULL;
+}
